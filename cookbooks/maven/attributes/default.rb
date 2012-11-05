3c512d1e4f9b7fe88fadd29497f9e09b0153829f
jeff <jeffra45@cs.brown.edu>
2012-11-05 17:18:18 -0500

    add maven, v3 cookbook

--- cookbooks/maven/attributes/default.rb
+++ cookbooks/maven/attributes/default.rb
@@ -20,7 +20,7 @@
 # limitations under the License.
 #
 
-default['maven']['version'] = 2
+default['maven']['version'] = 3
 default['maven']['m2_home'] = '/usr/local/maven'
 default['maven']['2']['url'] = "http://apache.mirrors.tds.net/maven/binaries/apache-maven-2.2.1-bin.tar.gz"
 default['maven']['2']['checksum'] = "b9a36559486a862abfc7fb2064fd1429f20333caae95ac51215d06d72c02d376"
